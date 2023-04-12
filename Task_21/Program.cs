d816d38c889ab59a55f567fb9dd5526b2032cdb4
VladimirTimokhin <Vladimir@Timokhin.su>
2023-04-12 04:02:13 +0300

    fixed task 21

--- Task_21/Program.cs
+++ Task_21/Program.cs
@@ -1,16 +1,16 @@
 ﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 Console.Clear();
-Console.Write("Введите координату x точки A не равную нулю: ");
+Console.Write("Введите координату x точки A: ");
 double Xa = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите координату Y точки A не равную нулю: ");
+Console.Write("Введите координату Y точки A: ");
 double Ya = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите координату Z точки A не равную нулю: ");
+Console.Write("Введите координату Z точки A: ");
 double Za = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите координату x точки B не равную нулю: ");
+Console.Write("Введите координату x точки B: ");
 double Xb = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите координату Y точки B не равную нулю: ");
+Console.Write("Введите координату Y точки B: ");
 double Yb = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите координату Z точки B не равную нулю: ");
+Console.Write("Введите координату Z точки B: ");
 double Zb = Convert.ToDouble(Console.ReadLine());
 double AB = Math.Sqrt((Xb - Xa) * (Xb - Xa) + (Yb - Ya) * (Yb - Ya) + (Zb - Za) * (Zb - Za));
 Console.WriteLine($"Расстояние равно {AB}");
